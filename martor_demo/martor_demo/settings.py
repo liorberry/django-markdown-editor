f3b9f90268bdde72437314982092d7659a340a22
Lior Berry <liorb@fb.com>
2021-06-11 23:10:17 -0700

    update settings

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
